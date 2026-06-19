8bf656eb4f9ce142a589ad32bfa406ef5541347a
Jack Yao <105488074+StarsExpress@users.noreply.github.com>
2026-06-18 22:47:16 -0500

    Added logo to game home page.

--- configs/display_config.py
+++ configs/display_config.py
@@ -1,5 +1,7 @@
 """All display configurations."""
 
+MAX_NAME_LEN = 50  # Maximal player name length.
+
 DEFAULT_PLAYER_NAME = "💸Benji Bucket🪣"  # If player doesn't enter name.
 
 DANGER_ZONE = {"lower": 12, "upper": 16}
